b5df5b49cee549469c102d96245343c07f5d003a
Joakim Nordling <joakim.nordling@gmail.com>
2021-04-02 09:24:43 +0300

    Reformat with black and isort

--- shooter_tut1.py
+++ shooter_tut1.py
@@ -7,40 +7,38 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Shooter')
+pygame.display.set_caption("Shooter")
 
 
 class Soldier(pygame.sprite.Sprite):
-	def __init__(self, x, y, scale):
-		pygame.sprite.Sprite.__init__(self)
-		img = pygame.image.load('img/player/Idle/0.png')
-		self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-		self.rect = self.image.get_rect()
-		self.rect.center = (x, y)
+    def __init__(self, x, y, scale):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load("img/player/Idle/0.png")
+        self.image = pygame.transform.scale(
+            img, (int(img.get_width() * scale), int(img.get_height() * scale))
+        )
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
 
-	def draw(self):
-		screen.blit(self.image, self.rect)
-
+    def draw(self):
+        screen.blit(self.image, self.rect)
 
 
 player = Soldier(200, 200, 3)
 player2 = Soldier(400, 200, 3)
 
 
-
 run = True
 while run:
 
-	
-	player.draw()
-	player2.draw()
+    player.draw()
+    player2.draw()
 
-	for event in pygame.event.get():
-		#quit game
-		if event.type == pygame.QUIT:
-			run = False
+    for event in pygame.event.get():
+        # quit game
+        if event.type == pygame.QUIT:
+            run = False
 
-
-	pygame.display.update()
+    pygame.display.update()
 
 pygame.quit()
